d156cc4b398c109e825b34b5e995db3885fd9bd6
sangeetha <112378439+sangeethaa1701@users.noreply.github.com>
2022-10-10 15:26:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,20 +20,23 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
-
-streamlit.dataframe(fruits_to_show)
-
+#create the repeatable code block(called a function)
+def get_fruitvice_data(this_fruit_choice):
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+    # write your own comment -what does the next line do? 
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    return fruityvice_normalized
+#New section to display fruityvice api response
+#streamlit.dataframe(fruits_to_show)
 streamlit.header('FruityVice Fruit Advice!')
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
        streamlit.error("Please select a fruit to get information.")
   else:
-       fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-       # write your own comment -what does the next line do? 
-       fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+       back_from_function = get_fruityvice_data(fruit_choice)
        # write your own comment - what does this do?
-       streamlit.dataframe(fruityvice_normalized)
+       streamlit.dataframe(back_from_function)
 except URLError as e:
     lstreamlit.error()
       
